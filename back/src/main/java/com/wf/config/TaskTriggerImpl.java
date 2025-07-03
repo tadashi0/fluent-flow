a62242cb4bdb2629180d125beb5925bd51a9575a
田重辉 <tadashio@163.com>
2025-07-03 10:46:52 +0800

    比较完善的版本了

--- back/src/main/java/com/wf/config/TaskTriggerImpl.java
+++ back/src/main/java/com/wf/config/TaskTriggerImpl.java
@@ -4,13 +4,13 @@ import com.aizuda.bpm.engine.TaskTrigger;
 import com.aizuda.bpm.engine.core.Execution;
 import com.aizuda.bpm.engine.model.NodeModel;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 public class TaskTriggerImpl implements TaskTrigger {
 
     @Override
-    public boolean execute(NodeModel nodeModel, Execution execution, Supplier<Boolean> finish) {
+    public boolean execute(NodeModel nodeModel, Execution execution, Function<Execution, Boolean> finish) {
         System.out.println("执行了触发器 args = " + nodeModel.getExtendConfig().get("args"));
-        return finish.get();
+        return finish.apply(execution);
     }
 }
