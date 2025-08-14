7e4d9346b6d644c4d0f62483146f2832dfd64e26
田重辉 <tadashio@163.com>
2025-08-14 09:57:53 +0800

    fix: 遵守开源协议, 去掉核心代码

--- fluent-flow-ui/src/components/WorkFlow/index.ts
+++
@@ -1,5 +0,0 @@
-import WorkFlowPro from './src/WorkFlowPro.vue'
-import PreviewFlow from './src/PreviewFlow.vue'
-import WorkFlow from './src/workFlow.vue'
-
-export { WorkFlowPro, PreviewFlow, WorkFlow}
